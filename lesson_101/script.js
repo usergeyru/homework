7c6b6d74ebc1312606262a6d5736a0b9aea66a6c
usergeyru <usergeyru@yandex.ru>
2025-03-20 16:08:00 +0500

    Add files via upload

---
+++ lesson_101/script.js
@@ -0,0 +1,47 @@
+// Первое задание
+
+// function sayHello(name) {
+//   return `Hello` + name
+// }
+
+// console.log(sayHello(' Oleg'));
+
+// Второе задание
+
+// let numbers = [3,64,12,345,45]
+//   function checkLargeNumbers (array) {
+//     for (let i = 0; i < array.length; i++) {
+//       if (array[i] > 10)
+//       console.log(array[i]);
+//     }    
+//   }
+// checkLargeNumbers(numbers)
+  
+// Третье задание
+
+  // function calculate(num1, num2, operator) {
+  //   if (operator === 'plus') {
+  //     return num1 + num2;
+  //   } else if (operator === 'minus') {
+  //     return num1 - num2;
+  //   } else if (operator === 'multiply') {
+  //     return num1 * num2;
+  //   } else if (operator === 'divide') {
+  //     return num1 / num2;
+  //   } else {
+  //     return 'Ошибка: неизвестный оператор'
+  //   }
+  // }
+  // console.log(calculate(100,-5,'divide'));
+  
+
+
+
+
+
+
+
+
+
+
+
